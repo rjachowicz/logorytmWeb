7174d9e654c62e32baef2ac7984aa08ab709cd7a
rjachowicz <robertjach74@gmail.com>
2026-04-14 15:54:02 +0200

    v.07.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.logorytm.com',
+
+  integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
